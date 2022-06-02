0d891917efaed2adacee8d88e1bee3765f8f12f8
Gaëtan Maisse <gaetanmaisse@gmail.com>
2022-06-02 10:00:48 +0200

    Generate APIM changelog for version APIM - 3.15.9

---
+++ .circleci/apim/changelog_pr.sh
@@ -0,0 +1,15 @@
+# Sanitize MILESTONE_VERSION
+MILESTONE_VERSION_SANITIZED=$(echo ${MILESTONE_VERSION} | sed -e 's/[^a-zA-Z0-9]//g')
+
+# Create a branch for the changelog PR
+git checkout -b "changelog_apim_pr_${MILESTONE_VERSION_SANITIZED}"
+
+git status
+
+git add ./CHANGELOG*.adoc
+
+git commit -m "Generate APIM changelog for version ${MILESTONE_VERSION_SANITIZED}"
+
+# If not DRY_RUN, push the branch to the remote
+git push --set-upstream origin "changelog_pr_${MILESTONE_VERSION_SANITIZED}"
+gh pr create --title "Changelog for: ${MILESTONE_VERSION}" --body "Generate APIM changelog for version ${MILESTONE_VERSION}" --reviewer "@gravitee-io/apim"
