20c295315a2c58144b3306c8911d4e227edbf791
Gaëtan Maisse <gaetanmaisse@gmail.com>
2022-06-02 14:05:02 +0200

    chore: rewrite job to compute changelog for APIM

---
+++ .circleci/apim/changelog_pr.sh
@@ -0,0 +1,13 @@
+# Sanitize MILESTONE_VERSION
+MILESTONE_VERSION_SANITIZED=$(echo ${MILESTONE_VERSION} | sed -e 's/[^a-zA-Z0-9]//g')
+
+# Create a branch for the changelog PR
+git checkout -b "changelog_apim_pr_${MILESTONE_VERSION_SANITIZED}"
+
+git status
+
+git add ./CHANGELOG*.adoc
+
+git commit -m "Generate changelog for version ${MILESTONE_VERSION}"
+git push --set-upstream origin "changelog_apim_pr_${MILESTONE_VERSION_SANITIZED}"
+gh pr create --title "Changelog for ${MILESTONE_VERSION}" --body "Generate changelog for version ${MILESTONE_VERSION}" --reviewer "@gravitee-io/apim"
